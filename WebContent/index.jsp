8d1641c52889bf0372bb538f490acc28ac88a122
JYS1 <46517021+JYS1@users.noreply.github.com>
2021-01-06 15:15:11 +0900

    2021.01.06_3
    UserDB만듬

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -7,6 +7,10 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+	mediapedia
+	movie
+	tv
+	book
+	user
 </body>
 </html>
---
+++ src/service/UserService.java
@@ -0,0 +1,17 @@
+package service;
+
+import java.util.List;
+import vo.UserVO;
+
+public interface UserService {
+	public List<UserVO> userList();
+	
+	public void userAdd(UserVO vo);
+	
+	public void userDelete(String email);
+	
+	public UserVO login(String email, String password);
+	
+	
+
+}
